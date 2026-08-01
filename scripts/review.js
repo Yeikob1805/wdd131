796b68ad35ee5caeb88246c110f833b6946a0501
Yeikob1805 <yeikob.andre1805@gmail.com>
2026-07-31 21:38:05 -0500

    form

---
+++ scripts/review.js
@@ -0,0 +1,11 @@
+document.querySelector("#currentYear").textContent = new Date().getFullYear();
+
+document.querySelector("#lastModified").textContent = `Last Modified: ${document.lastModified}`;
+
+let reviewCount = Number(localStorage.getItem("reviewCount")) || 0;
+
+reviewCount++;
+
+localStorage.setItem("reviewCount", reviewCount);
+
+document.querySelector("#reviewCount").textContent = reviewCount;
